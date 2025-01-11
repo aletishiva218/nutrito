cb7d5a3611f6f2db708d4a1d80db63c559767236
Shiva Aleti <aletishiva218@gmail.com>
2025-01-11 19:16:21 +0530

    removed axios package from index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import express from "express"
 import cors from "cors";
-import axios from "axios";
 
 import register from "./Routes/routes/register.js";
 import login from "./Routes/routes/login.js";
