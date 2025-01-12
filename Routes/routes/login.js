9e6850b52348e971e890e80ffc7d2cb52bf455c5
Shiva Aleti <aletishiva218@gmail.com>
2025-01-13 00:13:01 +0530

    added _id:0

--- Routes/routes/login.js
+++ Routes/routes/login.js
@@ -3,7 +3,7 @@ import {userModal} from "../../Database/Modal.js";
 const register = async (req,res) => {
     const {userId} = req.body;
    
-    const user = await userModal.findOne({userId:userId})
+    const user = await userModal.findOne({userId:userId},{_id:0})
     return res.status(200).json({statusCode:200,status:"Ok",message:"user login successfully",user:user})
 }
 
